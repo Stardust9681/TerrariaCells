4ff40ea6caaae14206c2f17604061252fc1f4b5a
Stardust <Stardust9681@gmail.com>
2024-07-10 21:19:24 -0500

    Disable natural life regen

---
+++ Common/ModPlayers/Regenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TerrariaCells.Common.ModPlayers
+{
+	public class Regenerator : ModPlayer
+	{
+		public override void NaturalLifeRegen(ref float regen)
+		{
+			regen = 0;
+		}
+	}
+}
