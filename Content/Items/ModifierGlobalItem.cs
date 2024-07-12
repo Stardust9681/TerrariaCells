a786f17650d52e2a99d92804d6f9089c9697fc89
Syviery <duckie747@hotmail.com>
2024-07-12 01:43:21 -0700

    Modifier System Prototype
    A quick save on the prototype for modifiers, still needs tweaking and polishing before pushing to main

---
+++ Content/Items/ModifierGlobalItem.cs
@@ -0,0 +1,144 @@
+﻿using Stubble.Core.Classes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.WorldBuilding;
+using TerrariaCells.Content;
+
+namespace ModTesting.Content.Items
+{
+    public class ModifierData
+    {
+        public string name;
+        public string description;
+
+        public ModifierData(string name, string description) 
+        {
+            this.name = name;
+            this.description = description;
+        }
+    }
+
+    public class ModifierSystem : ModSystem
+    {
+        public enum Modifier
+        {
+            Burning,
+            Electrified,
+            ExplodeOnHit
+        }
+
+        public static Dictionary<Modifier, ModifierData> ModifierInfo;
+
+        public override void Load()
+        {
+            ModifierInfo = new Dictionary<Modifier, ModifierData>();
+
+            ModifierInfo.Add(Modifier.Burning, new ModifierData("Burning", "Burn your target on hit."));
+            ModifierInfo.Add(Modifier.Electrified, new ModifierData("Electrified", "Electrocute your target on hit."));
+            ModifierInfo.Add(Modifier.ExplodeOnHit, new ModifierData("Exploding", "Explode your target on hit."));
+        }
+
+        public static ModifierData GetModifierData(Modifier modifier)
+        {
+            return ModifierInfo[modifier];
+        }
+    }
+
+    public class ModifierGlobalItem : GlobalItem
+    {
+        public List<ModifierSystem.Modifier> itemModifiers = new List<ModifierSystem.Modifier>();
+
+        public override bool InstancePerEntity => true;
+
+        // Only apply item levels to weapons
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+        {
+            return lateInstantiation && entity.damage > 0;
+        }
+
+        public override void PostReforge(Item item)
+        {
+            itemModifiers.Add(ModifierSystem.Modifier.ExplodeOnHit);
+        }
+
+        public void AddModifiers(List<ModifierSystem.Modifier> modifiers)
+        {
+            foreach(ModifierSystem.Modifier modifier in modifiers)
+            {
+                itemModifiers.Add(modifier);
+            }
+        }
+
+        public override void NetSend(Item item, BinaryWriter writer)
+        {
+            writer.Write(itemModifiers.Count);
+
+            List<int> integerList = itemModifiers.Select(x => Convert.ToInt32(x)).ToList();
+
+            for (int i = 0; i < integerList.Count; i++)
+            {
+                writer.Write(integerList[i]);
+            }
+        }
+
+        public override void NetReceive(Item item, BinaryReader reader)
+        {
+            int itemCount = reader.ReadInt32();
+
+            List<int> integerList = new List<int>();
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                integerList.Add(reader.ReadInt32());
+            }
+
+            List<ModifierSystem.Modifier> enumList = integerList.Select(x => (ModifierSystem.Modifier)Enum.Parse(typeof(ModifierSystem.Modifier), x.ToString())).ToList();
+            itemModifiers = enumList;
+        }
+
+        public override void SaveData(Item item, TagCompound tag)
+        {
+            List<int> integerList = itemModifiers.Select(x => Convert.ToInt32(x)).ToList();
+            tag.Add("modifiers", integerList);
+        }
+
+        public override void LoadData(Item item, TagCompound tag)
+        {
+            itemModifiers = new List<ModifierSystem.Modifier>();
+
+            List<ModifierSystem.Modifier> enumList = tag.GetList<int>("modifiers").Select(x => (ModifierSystem.Modifier)Enum.Parse(typeof(ModifierSystem.Modifier), x.ToString())).ToList();
+            itemModifiers = enumList;
+        }
+
+        public override GlobalItem Clone(Item item, Item itemClone)
+        {
+            ModifierGlobalItem myClone = (ModifierGlobalItem)base.Clone(item, itemClone);
+
+            myClone.itemModifiers = itemModifiers;
+
+            return myClone;
+        }
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (itemModifiers.Count >= 1)
+            {
+                foreach(ModifierSystem.Modifier modifier in itemModifiers)
+                {
+                    ModifierData data = ModifierSystem.GetModifierData(modifier);
+
+                    tooltips.Add(new TooltipLine(Mod, "Modifer", data.name + ": " + data.description ));
+                }
+
+            }
+        }
+    }
+}
---
+++ Content/NPCs/TerraCellsGlobalNpc.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using ModTesting.Content.Items;
+
+namespace ModTesting.Content.NPCs
+{
+    public class TerraCellsGlobalNpc : GlobalNPC
+    {
+
+        public override void OnHitByProjectile(NPC npc, Projectile projectile, NPC.HitInfo hit, int damageDone)
+        {
+            SourceGlobalProjectile testGlobalProjectile = null;
+            projectile.TryGetGlobalProjectile<SourceGlobalProjectile>(out testGlobalProjectile);
+
+            if (testGlobalProjectile != null)
+            {
+                TriggerOnHit(testGlobalProjectile.itemSource, npc);
+                //Mod.Logger.Debug(projectile.Name + " hit using a " + testGlobalProjectile.itemSource.Name + " with modifiers: ");
+            }
+
+            base.OnHitByProjectile(npc, projectile, hit, damageDone);
+        }
+
+        public override void OnHitByItem(NPC npc, Player player, Item item, NPC.HitInfo hit, int damageDone)
+        {
+            TriggerOnHit(item, npc);
+            //Mod.Logger.Debug(item.Name + " hit");
+
+            base.OnHitByItem(npc, player, item, hit, damageDone);
+        }
+
+        public void TriggerOnHit(Item sourceItem, NPC npc)
+        {
+            ModifierGlobalItem modifierGlobalItem;
+            sourceItem.TryGetGlobalItem<ModifierGlobalItem>(out modifierGlobalItem);
+
+            if (modifierGlobalItem != null)
+            {
+                switch (modifierGlobalItem)
+                {
+                    case var burning when modifierGlobalItem.itemModifiers.Contains(ModifierSystem.Modifier.Burning):
+                        Mod.Logger.Debug("BURN BABY BURN");
+                        break;
+                    case var explosion when modifierGlobalItem.itemModifiers.Contains(ModifierSystem.Modifier.ExplodeOnHit):
+                        Explosion(npc.Center, 20);
+                        break;
+                }
+            }
+        }
+
+        private void Explosion(Vector2 position, int size)
+        {
+
+            for (int i = 0; i < 15; i++)
+            {
+                int dust = Dust.NewDust(position, size, size, DustID.Smoke, 0f, 0f, 100, default, 1.7f);
+                Main.dust[dust].velocity *= 1.4f;
+            }
+            for (int i = 0; i < 27; i++)
+            {
+                int dust = Dust.NewDust(position, size, size, DustID.Torch, 0f, 0f, 100, default, 2.4f);
+                Main.dust[dust].noGravity = true;
+                Main.dust[dust].velocity *= 5f;
+                dust = Dust.NewDust(position, size, size, DustID.Torch, 0f, 0f, 100, default, 1.6f);
+                Main.dust[dust].velocity *= 3f;
+            }
+        }
+    }
+}
