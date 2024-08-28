d14f5dcbd012121c65177c3849022b51455eee73
Stardust <Stardust9681@gmail.com>
2024-08-27 19:08:30 -0500

    Hide large gems when held

---
+++ Common/UIPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.DataStructures;
+
+namespace TerrariaCells.Common
+{
+	public class UIPlayer : ModPlayer
+	{
+		public override void HideDrawLayers(PlayerDrawSet drawInfo)
+		{
+			PlayerDrawLayers.CaptureTheGem.Hide();
+		}
+	}
+}
