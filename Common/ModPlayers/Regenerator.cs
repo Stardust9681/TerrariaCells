0e0824c94bf07b8b01f4c58c9999433771250bfd
Stardust <Stardust9681@gmail.com>
2024-09-03 19:50:54 -0500

    Healthbar UI expands with statLifeMax2, to higher maximum

--- Common/ModPlayers/Regenerator.cs
+++ Common/ModPlayers/Regenerator.cs
@@ -9,6 +9,10 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria.GameContent.UI.ResourceSets;
 using ReLogic.Content;
 
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using System.Reflection;
+
 namespace TerrariaCells.Common.ModPlayers
 {
 	//ModPlayer handling health and regeneration aspects
@@ -17,10 +21,88 @@ namespace TerrariaCells.Common.ModPlayers
 		public override void Load()
 		{
 			On_ResourceDrawSettings.Draw += DrawRallyHealthBar;
+			//On_HorizontalBarsPlayerResourcesDisplaySet.Draw += DrawHealthbarContainer;
+			IL_HorizontalBarsPlayerResourcesDisplaySet.Draw += IL_HorizontalBarsPlayerResourcesDisplaySet_Draw;
+			//On_HorizontalBarsPlayerResourcesDisplaySet.LifeFillingDrawer += HealthbarTextureSelect;
+			IL_HorizontalBarsPlayerResourcesDisplaySet.LifeFillingDrawer += IL_HealthbarTextureSelect;
 		}
+
+		private static void IL_HealthbarTextureSelect(ILContext context)
+		{
+			try
+			{
+				log4net.ILog GetInstanceLogger() => ModContent.GetInstance<TerrariaCells>().Logger;
+				void Debug(object o) => GetInstanceLogger().Debug(o);
+
+				ILCursor cursor = new ILCursor(context);
+
+				ILLabel? IL_001B = null;
+				if (!cursor.TryGotoNext(MoveType.Before,
+					i => i.MatchLdarg1(),
+					i => i.MatchLdarg0(),
+					i => i.MatchLdfld<HorizontalBarsPlayerResourcesDisplaySet>("_hpFruitCount"),
+					i => i.Match(OpCodes.Bge_S, out IL_001B)))
+				{
+					Debug($"Couldn't match to IL_001B");
+					return;
+				}
+				cursor.Emit(OpCodes.Ldarg_1);
+				cursor.EmitDelegate<Func<int, bool>>((int elementIndex) =>
+				{
+					Player player = Main.LocalPlayer;
+					int segmentsX5WithBonusHealth = Math.Max((player.statLifeMax2 - MAX_HEALTHBAR_SIZE), 0);
+					return (elementIndex * 5) >= segmentsX5WithBonusHealth;
+				});
+				ILLabel jumpBack = cursor.MarkLabel();
+				if (!cursor.TryGotoNext(MoveType.Before,
+					i => i.MatchLdarg(4),
+					i => i.MatchLdarg0(),
+					i => i.MatchLdfld<HorizontalBarsPlayerResourcesDisplaySet>("_hpFillHoney"),
+					i => i.MatchStindRef()))
+				{
+					Debug($"Couldn't match to _hpFillHoney");
+					return;
+				}
+				ILLabel honeyFill = cursor.MarkLabel();
+				cursor.GotoLabel(jumpBack, MoveType.Before);
+				cursor.Emit(OpCodes.Brfalse, honeyFill);
+				cursor.Emit(OpCodes.Br, IL_001B);
+			}
+			catch (Exception x)
+			{
+				MonoModHooks.DumpIL(ModContent.GetInstance<TerrariaCells>(), context);
+			}
+		}
+
+		private void IL_HorizontalBarsPlayerResourcesDisplaySet_Draw(ILContext context)
+		{
+			try
+			{
+				ILCursor cursor = new ILCursor(context);
+				cursor.GotoNext(i => i.MatchCall<HorizontalBarsPlayerResourcesDisplaySet>("PrepareFields"));
+				cursor.Index++;
+				//Push HorizontalBars..DisplaySet instance being operated on, onto the Stack
+				cursor.Emit(OpCodes.Ldarg_0);
+				cursor.EmitDelegate((HorizontalBarsPlayerResourcesDisplaySet instance) =>
+				{
+					Player localPlayer = Main.LocalPlayer;
+
+					int numSegments = Math.Clamp(localPlayer.statLifeMax2 / 20, 5, MAX_HEALTHBAR_SIZE / 20);
+					typeof(HorizontalBarsPlayerResourcesDisplaySet).GetField("_hpSegmentsCount", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(instance, numSegments);
+				});
+			}
+			catch (Exception x)
+			{
+				MonoModHooks.DumpIL(Mod, context);
+			}
+		}
+
 		public override void Unload()
 		{
 			On_ResourceDrawSettings.Draw -= DrawRallyHealthBar;
+			IL_HorizontalBarsPlayerResourcesDisplaySet.Draw -= IL_HorizontalBarsPlayerResourcesDisplaySet_Draw;
+			//On_HorizontalBarsPlayerResourcesDisplaySet.Draw -= DrawHealthbarContainer;
+			IL_HorizontalBarsPlayerResourcesDisplaySet.LifeFillingDrawer -= IL_HealthbarTextureSelect;
 		}
 
 		public override IEnumerable<Item> AddStartingItems(bool mediumCoreDeath)
@@ -33,27 +115,9 @@ namespace TerrariaCells.Common.ModPlayers
 				};
 		}
 
-		#region Rally Heal Mechanic
-		public const float STAGGER_POTENCY = 3f;
-		public const float INV_STAGGER_POTENCY = 1f / STAGGER_POTENCY;
-		public const string BAR_HEALTH_FILL1 = "Images\\UI\\PlayerResourceSets\\HorizontalBars\\HP_Fill";
-		public const string BAR_HEALTH_FILL2 = "Images\\UI\\PlayerResourceSets\\HorizontalBars\\HP_Fill_Honey";
+		#region Healthbar
+		public const int MAX_HEALTHBAR_SIZE = 800;
 
-		private int damageBuffer;
-		private int damageTime;
-		private float antiRegen;
-
-		private float TimeAmplitude => damageBuffer * INV_STAGGER_POTENCY; //Used for calculations, opposite of MaxTime
-		private float MaxTime => damageBuffer * STAGGER_POTENCY; //Time it will take for damage to stop ticking.
-		private int DamageLeft => (int)(-MathF.Sqrt(TimeAmplitude * damageTime) + damageBuffer); //Remaining amount of damage for the player to take
-
-		//Mathematics used for Damage Staggering:
-			//Damage Left = -sqrt(TimeAmplitude * damageTime) + damageBuffer
-			//Damage per Tick = -TimeAmplitude / (2 * sqrt(TimeAmplitude * damageTime))
-		//Damage Left approaches 0 when damageTime reaches MaxTime
-
-		//Detour for Health Bar drawing
-		//Handles health indicator for Rally Mechanic
 		private void DrawRallyHealthBar(On_ResourceDrawSettings.orig_Draw orig, ref ResourceDrawSettings self, SpriteBatch spriteBatch, ref bool isHovered)
 		{
 			Player player = Main.LocalPlayer;
@@ -64,8 +128,9 @@ namespace TerrariaCells.Common.ModPlayers
 				return;
 			}
 
-			//Assuming that 20 segments is our cap here
-			int segmentSize = player.statLifeMax2 <= 400 ? 20 : (player.statLifeMax2 <= 500 ? 25 : (Player.statLifeMax2 / 20));
+			int segmentSize = player.statLifeMax2 <= MAX_HEALTHBAR_SIZE ? 20 : ((MAX_HEALTHBAR_SIZE * 20) / player.statLifeMax2);
+			if (segmentSize == 0)
+				segmentSize = 1;
 			int endPoint = player.statLife - modPlayer.DamageLeft;
 			if (endPoint < 0)
 				endPoint = 0;
@@ -81,7 +146,7 @@ namespace TerrariaCells.Common.ModPlayers
 			{
 				elementIndex++;
 				self.GetTextureMethod.Invoke(elementIndex, self.ElementIndexOffset, self.ElementIndexOffset + elementCount - 1, out Asset<Texture2D> asset, out Vector2 value3, out float scale, out Rectangle? rectangle);
-				if (i==0)
+				if (i == 0)
 				{
 					if (!(asset.Name.Equals(BAR_HEALTH_FILL1) || asset.Name.Equals(BAR_HEALTH_FILL2)))
 					{
@@ -137,6 +202,26 @@ namespace TerrariaCells.Common.ModPlayers
 				value += self.OffsetPerDraw + (rectangle2.Size() * self.OffsetPerDrawByTexturePercentile);
 			}
 		}
+		#endregion
+
+		#region Rally Heal Mechanic
+		public const float STAGGER_POTENCY = 3f;
+		public const float INV_STAGGER_POTENCY = 1f / STAGGER_POTENCY;
+		public const string BAR_HEALTH_FILL1 = "Images\\UI\\PlayerResourceSets\\HorizontalBars\\HP_Fill";
+		public const string BAR_HEALTH_FILL2 = "Images\\UI\\PlayerResourceSets\\HorizontalBars\\HP_Fill_Honey";
+
+		private int damageBuffer;
+		private int damageTime;
+		private float antiRegen;
+
+		private float TimeAmplitude => damageBuffer * INV_STAGGER_POTENCY; //Used for calculations, opposite of MaxTime
+		private float MaxTime => damageBuffer * STAGGER_POTENCY; //Time it will take for damage to stop ticking.
+		private int DamageLeft => (int)(-MathF.Sqrt(TimeAmplitude * damageTime) + damageBuffer); //Remaining amount of damage for the player to take
+
+		//Mathematics used for Damage Staggering:
+			//Damage Left = -sqrt(TimeAmplitude * damageTime) + damageBuffer
+			//Damage per Tick = -TimeAmplitude / (2 * sqrt(TimeAmplitude * damageTime))
+		//Damage Left approaches 0 when damageTime reaches MaxTime
 
 		/// <summary>
 		/// Set damage stagger to a flat amount. Will discard current amount.
