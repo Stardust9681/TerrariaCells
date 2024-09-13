d065a3fe275ce5b687ff367233526afb2bd2c1ad
Shipmans_AGLA <boyblob@gmail.com>
2024-09-13 15:21:31 -0700

    cultist enemies

---
+++ Content/Projectiles/IceArrow.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerrariaCells.Common.Utilities;
+
+namespace TerrariaCells.Content.Projectiles
+{
+    //shot by cultist archer
+    public class IceArrow : ModProjectile
+    {
+        public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.FrostArrow;
+        public override void SetDefaults()
+        {
+            Projectile.timeLeft = 300;
+            Projectile.width = Projectile.height = 10;
+            Projectile.hostile = true;
+            Projectile.friendly = false;
+            Projectile.extraUpdates = 2;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Asset<Texture2D> t = TextureAssets.Projectile[Type];
+            Main.EntitySpriteDraw(t.Value, Projectile.Center - Main.screenPosition, null, lightColor, Projectile.rotation, new Vector2(t.Width() / 2, t.Height() / 2), Projectile.scale, SpriteEffects.None);
+            return false;
+        }
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            Projectile.velocity.Y += 0.005f;
+            Dust d = Dust.NewDustDirect(Projectile.position, 10, 10, DustID.IceTorch);
+            d.velocity *= 0.9f;
+            d.noGravity = true;
+            base.AI();
+        }
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.DeerclopsIceAttack, Projectile.Center);
+            for (int i = 0; i < 3; i++)
+            {
+                Projectile.NewProjectileDirect(Projectile.GetSource_Death(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<IceSpike>(), TCellsUtils.ScaledHostileDamage(25), 1);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Dust d = Dust.NewDustDirect(Projectile.Hitbox.Left(), 10, 0, DustID.IceTorch, 0, 0);
+                d.velocity = new Vector2(Main.rand.NextFloat(-1, 1), Main.rand.NextFloat(-4, -2));
+            }
+            base.OnKill(timeLeft);
+        }
+    }
+}
---
+++ Content/Projectiles/IceSpike.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TerrariaCells.Common.Utilities;
+
+namespace TerrariaCells.Content.Projectiles
+{
+    //created on ground by ice arrow
+    public class IceSpike : ModProjectile
+    {
+        public override string Texture => "Terraria/Images/Extra_" + ExtrasID.CultistIceshard;
+        public override void SetDefaults()
+        {
+            Projectile.timeLeft = 100;
+            Projectile.width = Projectile.height = 20;
+            //Projectile.height = 40;
+            Projectile.hostile = true;
+            Projectile.friendly = false;
+            Projectile.hide = true;
+            Projectile.tileCollide = false;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Asset<Texture2D> t = TextureAssets.Projectile[Type];
+            int originY = 40;
+            if (Projectile.frame == 1) originY = 38;
+            if (Projectile.frame == 2) originY = 50;
+            Main.EntitySpriteDraw(t.Value, Projectile.Center - Main.screenPosition, new Rectangle(0, 74 * Projectile.frame, 24, 74), lightColor, Projectile.rotation, new Vector2(t.Width() / 2, originY), new Vector2(Projectile.scale, TCellsUtils.LerpFloat(0, 1, Projectile.ai[1], 1, TCellsUtils.LerpEasing.OutCubic)), SpriteEffects.None);
+            return false;
+        }
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            behindNPCsAndTiles.Add(index);
+        }
+        public override void AI()
+        {
+            if (Projectile.ai[0] == 0)
+            {
+                Vector2 tilePos = Projectile.Center;
+                for (int i = -16; i <= 16; i += 16)
+                {
+                    for (int j = -16; j <= 16; j += 16)
+                    {
+                        Vector2 pos = Projectile.Center + new Vector2(i, j);
+                        if (WorldGen.SolidTile(Main.tile[(pos).ToTileCoordinates()]) && (pos.Distance(Projectile.Center) < tilePos.Distance(Projectile.Center) || Projectile.Center == tilePos))
+                        {
+                            tilePos = pos;
+                        }
+                    }
+                }
+                Projectile.rotation = Projectile.AngleTo(tilePos) + Main.rand.NextFloat(MathHelper.ToRadians(-30), MathHelper.ToRadians(30)) + MathHelper.PiOver2;
+                int offX = 0;
+                int offY = 0;
+                if (tilePos.X < Projectile.Center.X)
+                {
+                    offX = 16;
+                }
+                if (tilePos.Y < Projectile.Center.Y)
+                {
+                    offY = 16;
+                }
+                Vector2 tileSnap = tilePos.ToTileCoordinates().ToWorldCoordinates(offX, offY);
+                if (Math.Abs(tilePos.X - Projectile.Center.X) > Math.Abs(tilePos.Y - Projectile.Center.Y))
+                {
+                    Projectile.Center = new Vector2(tileSnap.X, Projectile.Center.Y);
+                }
+                else
+                {
+                    
+                    Projectile.Center = new Vector2(Projectile.Center.X, tileSnap.Y);
+                }
+                Projectile.Center -= new Vector2(8, 0).RotatedBy(Projectile.AngleTo(tilePos));
+                Projectile.ai[0] = 1;
+                Projectile.frame = Main.rand.Next(0, 3);
+                
+            }
+            if (Projectile.ai[1] < 1 && Projectile.timeLeft > 50)
+            {
+                Projectile.ai[1] += 0.1f;
+            }
+            
+            base.AI();
+        }
+        public override void OnKill(int timeLeft)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Frost, 0, 0);
+                d.noGravity = true;
+            }
+            base.OnKill(timeLeft);
+        }
+    }
+}
