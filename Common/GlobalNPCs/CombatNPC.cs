a524812b2e1c10ce526e4b433ccad248c91bd0d0
Stardust <Stardust9681@gmail.com>
2024-12-07 12:34:20 -0600

    Most of Brain of Cthulhu redone [Incomplete]

--- Common/GlobalNPCs/CombatNPC.cs
+++ Common/GlobalNPCs/CombatNPC.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
+using Microsoft.Xna.Framework.Graphics;
+using TerrariaCells.Common.Utilities;
+
+using static TerrariaCells.Common.Utilities.NPCHelpers;
 
 namespace TerrariaCells.Common.GlobalNPCs
 {
@@ -18,5 +22,66 @@ namespace TerrariaCells.Common.GlobalNPCs
 		}
 
 		public static void ToggleContactDamage(NPC npc, bool value) => npc.GetGlobalNPC<CombatNPC>().allowContactDamage = value;
+
+		public override void SetDefaults(NPC entity)
+		{
+			switch (entity.type)
+			{
+				case NPCID.BrainofCthulhu:
+					entity.knockBackResist = 0f; //0 effect from knockback
+					break;
+			}
+		}
+
+		public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+		{
+			switch (npc.type)
+			{
+				case NPCID.BrainofCthulhu:
+					int timer = npc.Timer();
+
+					if (timer > NPCTypes.BrainOfCthulhu.XTimeStart - 60 && timer < NPCTypes.BrainOfCthulhu.XTimeStart)
+					{
+						Vector2 centre = NPCTypes.BrainOfCthulhu.GetArenaCentre(npc);
+						Vector2 offset = NPCTypes.BrainOfCthulhu.ArenaSize * 0.5f;
+						Vector2[] positions = new Vector2[] {
+						centre + (offset * -1),
+						centre + offset,
+						centre + (offset * new Vector2(1, -1)),
+						centre + (offset * new Vector2(-1, 1)),
+						};
+						for (int i = 0; i < positions.Length; i++)
+						{
+							//int opposingIndex = (2 * (i / 2)) - (i % 2) + 1;
+							//Vector2 opposingPosition = positions[opposingIndex];
+							Vector2 pos = positions[i];
+							Color c = Color.Red * 0.75f;
+							float res = NPCHelpers.Pack(pos.ToTileCoordinates16());
+							if (res.Equals(npc.ai[2]))
+							{
+								c = Color.Yellow * 0.75f;
+							}
+							Terraria.Utils.DrawLine(spriteBatch, pos, centre, c, Color.Transparent, ((NPCTypes.BrainOfCthulhu.XTimeStart - timer) / 60f) * 8);
+						}
+					}
+					if (NPCTypes.BrainOfCthulhu.XTimeStart < timer && timer < NPCTypes.BrainOfCthulhu.XTimeEnd)
+					{
+						float percent = TCellsUtils.GetLerpValue(timer, (NPCTypes.BrainOfCthulhu.XTimeEnd - NPCTypes.BrainOfCthulhu.XTimeStart), TCellsUtils.LerpEasing.InOutBack, NPCTypes.BrainOfCthulhu.XTimeStart, false);
+						Vector2 centre = NPCTypes.BrainOfCthulhu.GetArenaCentre(npc);
+						Vector2 size = NPCTypes.BrainOfCthulhu.ArenaSize;
+						Vector2 topLeft = centre - (size * 0.5f);
+						Vector2 botLeft = topLeft + new Vector2(0, size.Y);
+						Vector2 topRight = topLeft + new Vector2(size.X, 0);
+						Vector2 botRight = topLeft + size;
+						ReLogic.Content.Asset<Texture2D> brain = Terraria.GameContent.TextureAssets.Npc[NPCID.BrainofCthulhu];
+						spriteBatch.Draw(brain.Value, Vector2.Lerp(botLeft, topRight, percent) - (npc.Size * 0.5f) - screenPos, npc.frame, drawColor * npc.Opacity);
+						spriteBatch.Draw(brain.Value, Vector2.Lerp(botRight, topLeft, percent) - (npc.Size * 0.5f) - screenPos, npc.frame, drawColor * npc.Opacity);
+						spriteBatch.Draw(brain.Value, Vector2.Lerp(topRight, botLeft, percent) - (npc.Size * 0.5f) - screenPos, npc.frame, drawColor * npc.Opacity);
+					}
+					return true;
+				default:
+					return base.PreDraw(npc, spriteBatch, screenPos, drawColor);
+			}
+		}
 	}
 }
--- Common/Utilities/NPCHelpers.cs
+++ Common/Utilities/NPCHelpers.cs
@@ -10,20 +10,29 @@ namespace TerrariaCells.Common.Utilities
 {
 	public static class NPCHelpers
 	{
-		public static int Timer(this NPC npc, int? value = null)
-		{
-			return (int)(npc.ai[0] = value ?? (int)npc.ai[0]);
-		}
-		public static int Phase(this NPC npc, int? value = null)
-		{
-			if (value is null)
-			{
-				return (int)npc.ai[1];
-			}
-			npc.ai[1] = value.Value;
-			npc.Timer(0); //Switching to new phase, reset phase timer
-			return (int)npc.ai[1];
-		}
+		#region Timer and Phase
+		//Packing Timer and Phase data into one variable
+		//Timer goes up to 18 minutes (ushort.MaxValue ticks), surely we don't need more than that
+		//There's more than enough space for phases
+		//This frees up at least one slot in the ai[] array for other use
+
+		public static void Timer(this NPC npc, int value)
+			=> npc.ai[0] = Pack((ushort)value, Unpack(npc.ai[0]).Item2);
+		public static ushort Timer(this NPC npc)
+			=> Unpack(npc.ai[0]).Item1;
+		public static void DoTimer(this NPC npc, int inc = 1)
+			=> npc.Timer(npc.Timer() + inc);
+
+		public static void Phase(this NPC npc, int value)
+			=> npc.ai[0] = value; //Roughly equivalent to 'Pack(0, (short)value);' which resets Timer upon phase change
+		public static ushort Phase(this NPC npc)
+			=> Unpack(npc.ai[0]).Item2;
+
+		internal static int Pack(ushort a, ushort b) => ((ushort)a << 16) | (ushort)b;
+		internal static int Pack(Terraria.DataStructures.Point16 point) => ((ushort)point.X << 16) | (ushort)point.Y;
+		internal static (ushort, ushort) Unpack(float value) => Unpack((int)value);
+		internal static (ushort, ushort) Unpack(int value) => ((ushort)((value >> 16) & ushort.MaxValue), (ushort)(value & ushort.MaxValue));
+		#endregion
 
 		///<returns>True if NPC "can see" a given position. False otherwise.</returns>
 		public static bool LineOfSight(this NPC npc, Vector2 toPosition)
@@ -33,12 +42,14 @@ namespace TerrariaCells.Common.Utilities
 		public static bool Grounded(this NPC npc)
 			=> npc.collideY && npc.oldVelocity.Y > npc.velocity.Y && npc.oldVelocity.Y > 0;
 
-		//Relocated and refactored from Fighters.cs\
-		///<returns>False if NPC has no target. True if NPC direction is towards player target</returns>
+		//Relocated and refactored from Fighters.cs
+		///<returns>False if NPC has no target. True if NPC direction is towards target</returns>
 		public static bool IsFacingTarget(this NPC npc)
 		{
 			if (!npc.HasValidTarget) return false;
-			return npc.direction == MathF.Sign(Main.player[npc.target].position.X - npc.position.X);
+			if (npc.SupportsNPCTargets && npc.HasNPCTarget) return npc.direction == MathF.Sign(Main.npc[npc.TranslatedTargetIndex].position.X - npc.position.X);
+			if (npc.HasPlayerTarget) return npc.direction == MathF.Sign(Main.player[npc.target].position.X - npc.position.X);
+			return false;
 		}
 		///<returns>True if NPC direction is towards target. False otherwise</returns>
 		public static bool IsFacingTarget(this NPC npc, Player target)
