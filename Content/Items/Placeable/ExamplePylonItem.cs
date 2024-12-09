56d1a5ef5b81d1b5ef4ba858ea4b779257aa52fa
Jarten <Artemy2MA@outlook.com>
2024-12-09 01:37:19 -0800

    finished with the alpha build

--- Content/Items/Placeable/ExamplePylonItem.cs
+++ Content/Items/Placeable/ExamplePylonItem.cs
@@ -6,15 +6,16 @@ namespace TerrariaCells.Content.Items.Placeable
 {
 	/// <summary>
 	/// The coupled item that places the Example Pylon tile. For more information on said tile,
-	/// see <seealso cref="ExamplePylonTile"/>.
+	/// see <seealso cref="ForestExitPylon"/>.
 	/// </summary>
 	public class ExamplePylonItem : ModItem
 	{
+
 		public override void SetDefaults()
 		{
 			// Basically, this a just a shorthand method that will set all default values necessary to place
 			// the passed in tile type; in this case, the Example Pylon tile.
-			Item.DefaultToPlaceableTile(ModContent.TileType<ExamplePylonTile>());
+			Item.DefaultToPlaceableTile(ModContent.TileType<ForestExitPylon>());
 
 			// Another shorthand method that will set the rarity and how much the item is worth.
 			Item.SetShopValues(ItemRarityColor.Blue1, Terraria.Item.buyPrice(gold: 10));
--- Content/Tiles/ExamplePylonTile.cs
+++ Content/Tiles/ExamplePylonTile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
@@ -16,6 +17,44 @@ using TerrariaCells.Content.TileEntities;
 
 namespace TerrariaCells.Content.Tiles;
 
+public class TeleportTracker : ModSystem
+{
+    public int teleports = 0;
+
+    public override void OnModLoad()
+    {
+        // On_Player.UpdateDead += Reset;
+
+        base.OnModLoad();
+    }
+
+    public void Reset(On_Player.orig_UpdateDead orig, Player player)
+    {
+        teleports = 0;
+    }
+
+    public override void OnWorldLoad()
+    {
+        teleports = 0;
+
+
+        base.OnWorldLoad();
+    }
+
+    public void Teleport()
+    {
+        teleports += 1;
+        switch (teleports)
+        {
+            case 2: Main.LocalPlayer.Teleport(new Vector2(32461f, 7814f)); return; //desert
+            case 4: Main.LocalPlayer.Teleport(new Vector2(47403f, 7158f)); return; //hive
+            case 6: Main.LocalPlayer.Teleport(new Vector2(56771f, 6790f)); return; //ice
+            case 8: Main.LocalPlayer.Teleport(new Vector2(8771f, 6102f)); teleports = 0; return; //forest
+        }
+        Main.LocalPlayer.Teleport(new Vector2(19623f, 10326f)); //inn
+    }
+}
+
 /// <summary>
 /// An example for creating a Pylon, identical to how they function in Vanilla. Shows off <seealso cref="ModPylon"/>, an abstract
 /// extension of <seealso cref="ModTile"/> that has additional functionality for Pylon specific tiles.
@@ -25,7 +64,7 @@ namespace TerrariaCells.Content.Tiles;
 /// override in order to streamline that process.)
 /// </br>
 /// </summary>
-public class ExamplePylonTile : ModPylon, ITerraCellsCategorization
+public class ForestExitPylon : ModPylon, ITerraCellsCategorization
 {
     public const int CrystalVerticalFrameCount = 8;
 
@@ -34,6 +73,198 @@ public class ExamplePylonTile : ModPylon, ITerraCellsCategorization
     public Asset<Texture2D> mapIcon;
 
     public TerraCellsItemCategory Category => TerraCellsItemCategory.Storage;
+
+    public override string Texture => (GetType().Namespace + "." + "ExamplePylonTile").Replace('.', '/');
+
+    public override void Load()
+    {
+        // We'll need these textures for later, it's best practice to cache them on load instead of continually requesting every draw call.
+        crystalTexture = ModContent.Request<Texture2D>(Texture + "_Crystal");
+        crystalHighlightTexture = ModContent.Request<Texture2D>(Texture + "_CrystalHighlight");
+        mapIcon = ModContent.Request<Texture2D>(Texture + "_MapIcon");
+
+    }
+
+    public override void SetStaticDefaults()
+    {
+        Main.tileLighted[Type] = true;
+        Main.tileFrameImportant[Type] = true;
+
+        TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
+        TileObjectData.newTile.LavaDeath = false;
+        TileObjectData.newTile.DrawYOffset = 2;
+        TileObjectData.newTile.StyleHorizontal = true;
+        // These definitions allow for vanilla's pylon TileEntities to be placed.
+        // tModLoader has a built in Tile Entity specifically for modded pylons, which we must extend (see SimplePylonTileEntity)
+        TEModdedPylon moddedPylon = ModContent.GetInstance<SimplePylonTileEntity>();
+        TileObjectData.newTile.HookCheckIfCanPlace = new PlacementHook(moddedPylon.PlacementPreviewHook_CheckIfCanPlace, 1, 0, true);
+        TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(moddedPylon.Hook_AfterPlacement, -1, 0, false);
+
+        TileObjectData.addTile(Type);
+
+        TileID.Sets.InteractibleByNPCs[Type] = true;
+        TileID.Sets.PreventsSandfall[Type] = true;
+        TileID.Sets.AvoidedByMeteorLanding[Type] = true;
+
+        // Adds functionality for proximity of pylons; if this is true, then being near this tile will count as being near a pylon for the teleportation process.
+        // AddToArray(ref TileID.Sets.CountsAsPylon);
+
+        // LocalizedText pylonName = CreateMapEntryName(); //Name is in the localization file
+        // AddMapEntry(Color.White, pylonName);
+    }
+
+    public override NPCShop.Entry GetNPCShopEntry()
+    {
+        return null;
+    }
+
+    public override void MouseOver(int i, int j)
+    {
+        Main.LocalPlayer.cursorItemIconEnabled = true;
+        Main.LocalPlayer.cursorItemIconText = "Leave";
+    }
+
+    public override void KillMultiTile(int i, int j, int frameX, int frameY)
+    {
+        ModContent.GetInstance<SimplePylonTileEntity>().Kill(i, j);
+    }
+
+    public override bool ValidTeleportCheck_NPCCount(TeleportPylonInfo pylonInfo, int defaultNecessaryNPCCount)
+    {
+        return true;
+    }
+
+    public override bool ValidTeleportCheck_BiomeRequirements(TeleportPylonInfo pylonInfo, SceneMetrics sceneData)
+    {
+        return false;
+    }
+
+    public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+    {
+        r = 0.02f;
+        g = b = 0.75f;
+
+    }
+
+    public override void SpecialDraw(int i, int j, SpriteBatch spriteBatch)
+    {
+        DefaultDrawPylonCrystal(spriteBatch, i, j, crystalTexture, crystalHighlightTexture, new Vector2(0f, -12f), Color.White * 0.1f, Color.White, 1, CrystalVerticalFrameCount);
+    }
+
+    public override bool RightClick(int i, int j)
+    {
+        Mod.GetContent<TeleportTracker>().First().Teleport();
+        // return base.RightClick(i, j);
+        return true;
+    }
+}
+
+public class DesertExitPylon : ModPylon, ITerraCellsCategorization
+{
+    public const int CrystalVerticalFrameCount = 8;
+
+    public Asset<Texture2D> crystalTexture;
+    public Asset<Texture2D> crystalHighlightTexture;
+    public Asset<Texture2D> mapIcon;
+
+    public TerraCellsItemCategory Category => TerraCellsItemCategory.Storage;
+    public override string Texture => (GetType().Namespace + "." + "ExamplePylonTile").Replace('.', '/');
+
+    public override void Load()
+    {
+        // We'll need these textures for later, it's best practice to cache them on load instead of continually requesting every draw call.
+        crystalTexture = ModContent.Request<Texture2D>(Texture + "_Crystal");
+        crystalHighlightTexture = ModContent.Request<Texture2D>(Texture + "_CrystalHighlight");
+        mapIcon = ModContent.Request<Texture2D>(Texture + "_MapIcon");
+    }
+
+    public override void SetStaticDefaults()
+    {
+        Main.tileLighted[Type] = true;
+        Main.tileFrameImportant[Type] = true;
+
+        TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
+        TileObjectData.newTile.LavaDeath = false;
+        TileObjectData.newTile.DrawYOffset = 2;
+        TileObjectData.newTile.StyleHorizontal = true;
+        // These definitions allow for vanilla's pylon TileEntities to be placed.
+        // tModLoader has a built in Tile Entity specifically for modded pylons, which we must extend (see SimplePylonTileEntity)
+        TEModdedPylon moddedPylon = ModContent.GetInstance<SimplePylonTileEntity>();
+        // bad return set to -1 to disable the pylon placement limit
+        TileObjectData.newTile.HookCheckIfCanPlace = new PlacementHook(moddedPylon.PlacementPreviewHook_CheckIfCanPlace, -1, 0, true);
+        TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(moddedPylon.Hook_AfterPlacement, -1, 0, false);
+
+
+
+        TileObjectData.addTile(Type);
+
+        TileID.Sets.InteractibleByNPCs[Type] = true;
+        TileID.Sets.PreventsSandfall[Type] = true;
+        TileID.Sets.AvoidedByMeteorLanding[Type] = true;
+
+        // Adds functionality for proximity of pylons; if this is true, then being near this tile will count as being near a pylon for the teleportation process.
+        // AddToArray(ref TileID.Sets.CountsAsPylon);
+
+        // LocalizedText pylonName = CreateMapEntryName(); //Name is in the localization file
+        // AddMapEntry(Color.White, pylonName);
+    }
+
+    public override NPCShop.Entry GetNPCShopEntry()
+    {
+        return null;
+    }
+
+    public override void MouseOver(int i, int j)
+    {
+        Main.LocalPlayer.cursorItemIconEnabled = true;
+        Main.LocalPlayer.cursorItemIconText = "Leave";
+    }
+
+    public override void KillMultiTile(int i, int j, int frameX, int frameY)
+    {
+        ModContent.GetInstance<SimplePylonTileEntity>().Kill(i, j);
+    }
+
+    public override bool ValidTeleportCheck_NPCCount(TeleportPylonInfo pylonInfo, int defaultNecessaryNPCCount)
+    {
+        return true;
+    }
+
+    public override bool ValidTeleportCheck_BiomeRequirements(TeleportPylonInfo pylonInfo, SceneMetrics sceneData)
+    {
+        return false;
+    }
+
+    public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+    {
+        r = 0.02f;
+        g = b = 0.75f;
+
+    }
+
+    public override void SpecialDraw(int i, int j, SpriteBatch spriteBatch)
+    {
+        DefaultDrawPylonCrystal(spriteBatch, i, j, crystalTexture, crystalHighlightTexture, new Vector2(0f, -12f), Color.White * 0.1f, Color.White, 1, CrystalVerticalFrameCount);
+    }
+
+    public override bool RightClick(int i, int j)
+    {
+        Mod.GetContent<TeleportTracker>().First().Teleport();
+        // return base.RightClick(i, j);
+        return true;
+    }
+}
+
+public class HiveExitPylon : ModPylon, ITerraCellsCategorization
+{
+    public const int CrystalVerticalFrameCount = 8;
+
+    public Asset<Texture2D> crystalTexture;
+    public Asset<Texture2D> crystalHighlightTexture;
+    public Asset<Texture2D> mapIcon;
+
+    public TerraCellsItemCategory Category => TerraCellsItemCategory.Storage;
+    public override string Texture => (GetType().Namespace + "." + "ExamplePylonTile").Replace('.', '/');
 
     public override void Load()
     {
@@ -65,62 +296,40 @@ public class ExamplePylonTile : ModPylon, ITerraCellsCategorization
         TileID.Sets.AvoidedByMeteorLanding[Type] = true;
 
         // Adds functionality for proximity of pylons; if this is true, then being near this tile will count as being near a pylon for the teleportation process.
-        AddToArray(ref TileID.Sets.CountsAsPylon);
+        // AddToArray(ref TileID.Sets.CountsAsPylon);
 
-        LocalizedText pylonName = CreateMapEntryName(); //Name is in the localization file
-        AddMapEntry(Color.White, pylonName);
+        // LocalizedText pylonName = CreateMapEntryName(); //Name is in the localization file
+        // AddMapEntry(Color.White, pylonName);
     }
 
     public override NPCShop.Entry GetNPCShopEntry()
     {
-        // In this method we can customize the shop entry for the pylon item.
-        // The default method, base.GetNPCShopEntry(), generates a shop entry for the pylon item with the typical pylon conditions: Condition.HappyEnoughToSellPylons, Condition.AnotherTownNPCNearby, and Condition.NotInEvilBiome
-        // NPCShop.Entry shopEntry = base.GetNPCShopEntry();
-
-        // We will take that shop entry and add an additional condition to check for ExampleBiome, as this is typical for biome pylons
-        // This does not affect the teleport conditions, only the sale conditions
-        // shopEntry.AddCondition(ExampleConditions.InExampleBiome);
-
-        // and finally we return the shop entry
         return null;
     }
 
     public override void MouseOver(int i, int j)
     {
-        // Show a little pylon icon on the mouse indicating we are hovering over it.
         Main.LocalPlayer.cursorItemIconEnabled = true;
-        // Main.LocalPlayer.cursorItemIconID = ModContent.ItemType<ExamplePylonItem>();
         Main.LocalPlayer.cursorItemIconText = "Leave";
     }
 
     public override void KillMultiTile(int i, int j, int frameX, int frameY)
     {
-        // We need to clean up after ourselves, since this is still a "unique" tile, separate from Vanilla Pylons, so we must kill the TileEntity.
         ModContent.GetInstance<SimplePylonTileEntity>().Kill(i, j);
     }
 
     public override bool ValidTeleportCheck_NPCCount(TeleportPylonInfo pylonInfo, int defaultNecessaryNPCCount)
     {
-        // Let's say for fun sake that no NPCs need to be nearby in order for this pylon to function. If you want your pylon to function just like vanilla,
-        // you don't need to override this method at all.
         return true;
     }
 
     public override bool ValidTeleportCheck_BiomeRequirements(TeleportPylonInfo pylonInfo, SceneMetrics sceneData)
     {
-        // Right before this hook is called, the sceneData parameter exports its information based on wherever the destination pylon is,
-        // and by extension, it will call ALL ModSystems that use the TileCountsAvailable method. This means, that if you determine biomes
-        // based off of tile count, when this hook is called, you can simply check the tile threshold, like we do here. In the context of ExampleMod,
-        // something is considered within the Example Surface/Underground biome if there are 40 or more example blocks at that location.
-
-        // return ModContent.GetInstance<ExampleBiomeTileCount>().exampleBlockCount >= 40;
         return false;
     }
 
     public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
     {
-        // Pylons in vanilla light up, which is just a simple functionality we add using ModTile's ModifyLight.
-        // Let's just add a simple white light for our pylon:
         r = 0.02f;
         g = b = 0.75f;
 
@@ -128,22 +337,105 @@ public class ExamplePylonTile : ModPylon, ITerraCellsCategorization
 
     public override void SpecialDraw(int i, int j, SpriteBatch spriteBatch)
     {
-        // We want to draw the pylon crystal the exact same way vanilla does, so we can use this built in method in ModPylon for default crystal drawing:
-        // For the sake of example, lets make our pylon create a bit more dust by decreasing the dustConsequent value down to 1. If you want your dust spawning to be identical to vanilla, set dustConsequent to 4.
-        // We also multiply the pylonShadowColor in order to decrease its opacity, so it actually looks like a "shadow"
         DefaultDrawPylonCrystal(spriteBatch, i, j, crystalTexture, crystalHighlightTexture, new Vector2(0f, -12f), Color.White * 0.1f, Color.White, 1, CrystalVerticalFrameCount);
-    }
-
-    public override void DrawMapIcon(ref MapOverlayDrawContext context, ref string mouseOverText, TeleportPylonInfo pylonInfo, bool isNearPylon, Color drawColor, float deselectedScale, float selectedScale)
-    {
-        // Just like in SpecialDraw, we want things to be handled the EXACT same way vanilla would handle it, which ModPylon also has built in methods for:
-        // bool mouseOver = DefaultDrawMapIcon(ref context, mapIcon, pylonInfo.PositionInTiles.ToVector2() + new Vector2(1.5f, 2f), drawColor, deselectedScale, selectedScale);
-        // DefaultMapClickHandle(mouseOver, pylonInfo, ModContent.GetInstance<ExamplePylonItem>().DisplayName.Key, ref mouseOverText);
     }
 
     public override bool RightClick(int i, int j)
     {
-        Main.LocalPlayer.Teleport(new Vector2(200f, 200f));
+        Mod.GetContent<TeleportTracker>().First().Teleport();
+        // return base.RightClick(i, j);
+        return true;
+    }
+}
+
+public class SnowExitPylon : ModPylon, ITerraCellsCategorization
+{
+    public const int CrystalVerticalFrameCount = 8;
+
+    public Asset<Texture2D> crystalTexture;
+    public Asset<Texture2D> crystalHighlightTexture;
+    public Asset<Texture2D> mapIcon;
+
+    public TerraCellsItemCategory Category => TerraCellsItemCategory.Storage;
+    public override string Texture => (GetType().Namespace + "." + "ExamplePylonTile").Replace('.', '/');
+
+    public override void Load()
+    {
+        // We'll need these textures for later, it's best practice to cache them on load instead of continually requesting every draw call.
+        crystalTexture = ModContent.Request<Texture2D>(Texture + "_Crystal");
+        crystalHighlightTexture = ModContent.Request<Texture2D>(Texture + "_CrystalHighlight");
+        mapIcon = ModContent.Request<Texture2D>(Texture + "_MapIcon");
+    }
+
+    public override void SetStaticDefaults()
+    {
+        Main.tileLighted[Type] = true;
+        Main.tileFrameImportant[Type] = true;
+
+        TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
+        TileObjectData.newTile.LavaDeath = false;
+        TileObjectData.newTile.DrawYOffset = 2;
+        TileObjectData.newTile.StyleHorizontal = true;
+        // These definitions allow for vanilla's pylon TileEntities to be placed.
+        // tModLoader has a built in Tile Entity specifically for modded pylons, which we must extend (see SimplePylonTileEntity)
+        TEModdedPylon moddedPylon = ModContent.GetInstance<SimplePylonTileEntity>();
+        TileObjectData.newTile.HookCheckIfCanPlace = new PlacementHook(moddedPylon.PlacementPreviewHook_CheckIfCanPlace, 1, 0, true);
+        TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(moddedPylon.Hook_AfterPlacement, -1, 0, false);
+
+        TileObjectData.addTile(Type);
+
+        TileID.Sets.InteractibleByNPCs[Type] = true;
+        TileID.Sets.PreventsSandfall[Type] = true;
+        TileID.Sets.AvoidedByMeteorLanding[Type] = true;
+
+        // Adds functionality for proximity of pylons; if this is true, then being near this tile will count as being near a pylon for the teleportation process.
+        // AddToArray(ref TileID.Sets.CountsAsPylon);
+
+        // LocalizedText pylonName = CreateMapEntryName(); //Name is in the localization file
+        // AddMapEntry(Color.White, pylonName);
+    }
+
+    public override NPCShop.Entry GetNPCShopEntry()
+    {
+        return null;
+    }
+
+    public override void MouseOver(int i, int j)
+    {
+        Main.LocalPlayer.cursorItemIconEnabled = true;
+        Main.LocalPlayer.cursorItemIconText = "Leave";
+    }
+
+    public override void KillMultiTile(int i, int j, int frameX, int frameY)
+    {
+        ModContent.GetInstance<SimplePylonTileEntity>().Kill(i, j);
+    }
+
+    public override bool ValidTeleportCheck_NPCCount(TeleportPylonInfo pylonInfo, int defaultNecessaryNPCCount)
+    {
+        return true;
+    }
+
+    public override bool ValidTeleportCheck_BiomeRequirements(TeleportPylonInfo pylonInfo, SceneMetrics sceneData)
+    {
+        return false;
+    }
+
+    public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+    {
+        r = 0.02f;
+        g = b = 0.75f;
+
+    }
+
+    public override void SpecialDraw(int i, int j, SpriteBatch spriteBatch)
+    {
+        DefaultDrawPylonCrystal(spriteBatch, i, j, crystalTexture, crystalHighlightTexture, new Vector2(0f, -12f), Color.White * 0.1f, Color.White, 1, CrystalVerticalFrameCount);
+    }
+
+    public override bool RightClick(int i, int j)
+    {
+        Mod.GetContent<TeleportTracker>().First().Teleport();
         // return base.RightClick(i, j);
         return true;
     }
