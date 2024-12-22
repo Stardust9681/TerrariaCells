4a705dfe0b5dd26ad5f6767e0f5df2bf89a56a01
Stardust <Stardust9681@gmail.com>
2024-12-22 13:21:07 -0600

    No tile interactions

--- Common/GlobalTiles/VanillaReworksGlobalTile.cs
+++ Common/GlobalTiles/VanillaReworksGlobalTile.cs
@@ -1,4 +1,7 @@
-﻿using Terraria;
+﻿using System;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace TerrariaCells.Common.GlobalTiles
@@ -8,10 +11,40 @@ namespace TerrariaCells.Common.GlobalTiles
         public override void Load()
         {
             On_Projectile.ExplodeTiles += On_Projectile_ExplodeTiles;
+			On_Player.TileInteractionsUse += On_Player_TileInteractionsUse;
+			On_Player.InInteractionRange += On_Player_InInteractionRange;
         }
 
-        // Prevent explosions from destroying tiles
-        private void On_Projectile_ExplodeTiles(On_Projectile.orig_ExplodeTiles orig, Projectile self, Vector2 compareSpot, int radius, int minI, int maxI, int minJ, int maxJ, bool wallSplode)
+		private bool On_Player_InInteractionRange(On_Player.orig_InInteractionRange orig, Player self, int interactX, int interactY, Terraria.DataStructures.TileReachCheckSettings settings)
+		{
+			Tile tile = Framing.GetTileSafely(interactX, interactY);
+			if (ValidTiles.Contains(tile.TileType))
+			{
+				if (tile.TileType == TileID.TeleportationPylon)
+				{
+					settings.OverrideXReach = Systems.WorldPylonSystem.MAX_PYLON_RANGE;
+					settings.OverrideYReach = Systems.WorldPylonSystem.MAX_PYLON_RANGE;
+				}
+				return orig.Invoke(self, interactX, interactY, settings);
+			}
+			return false;
+		}
+
+		private readonly ushort[] ValidTiles = new ushort[] {
+			TileID.TeleportationPylon,
+			TileID.GemLocks,
+		};
+		private void On_Player_TileInteractionsUse(On_Player.orig_TileInteractionsUse orig, Player self, int myX, int myY)
+		{
+			Tile tile = Framing.GetTileSafely(myX, myY);
+			if (ValidTiles.Contains(tile.TileType))
+			{
+				orig.Invoke(self, myX, myY);
+			}
+		}
+
+		// Prevent explosions from destroying tiles
+		private void On_Projectile_ExplodeTiles(On_Projectile.orig_ExplodeTiles orig, Projectile self, Vector2 compareSpot, int radius, int minI, int maxI, int minJ, int maxJ, bool wallSplode)
         {
             return;
         }
