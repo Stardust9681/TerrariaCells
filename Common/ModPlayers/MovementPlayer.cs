6a0e705d1a3fab0605abb263fecff320ee4da727
Stardust <Stardust9681@gmail.com>
2024-12-22 13:36:05 -0600

    Remove fall damage

---
+++ Common/ModPlayers/MovementPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace TerrariaCells.Common.ModPlayers
+{
+	public class MovementPlayer : ModPlayer
+	{
+		public override void ResetEffects()
+		{
+			Player.noFallDmg = true;
+			//Un-comment if we want to give player Cloud Jump
+			//Player.GetJumpState(VanillaExtraJump.CloudInABottle).Enable();
+		}
+	}
+}
