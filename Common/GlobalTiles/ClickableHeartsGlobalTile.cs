2c2871ab35fab24ab4194b879a3d7015f19dfecf
Jarten <Artemy2MA@outlook.com>
2024-12-30 18:21:47 -0800

    tweaked pickup to heal hp on pickup

--- Common/GlobalTiles/ClickableHeartsGlobalTile.cs
+++ Common/GlobalTiles/ClickableHeartsGlobalTile.cs
@@ -4,21 +4,32 @@ using Terraria.ModLoader;
 using Terraria.ID;
 using TerrariaCells.Common.ModPlayers;
 
-namespace TerrariaCells.Common.GlobalTiles {
-	public class ClickcableHeartsGlobalTile : GlobalTile {
-		public override void RightClick(int i, int j, int type) {
-			if (type == TileID.Heart) {
+namespace TerrariaCells.Common.GlobalTiles
+{
+	public class ClickcableHeartsGlobalTile : GlobalTile
+	{
+		public override void RightClick(int i, int j, int type)
+		{
+			if (type == TileID.Heart)
+			{
 				WorldGen.KillTile(i, j);
 				Main.player[Main.myPlayer].GetModPlayer<LifeModPlayer>().extraHealth += 20;
-			} else {
+				Main.LocalPlayer.Heal(20);
+			}
+			else
+			{
 				base.RightClick(i, j, type);
 			}
 
 		}
-		public override bool CanDrop(int i, int j, int type) {
-			if (type == TileID.Heart) {
+		public override bool CanDrop(int i, int j, int type)
+		{
+			if (type == TileID.Heart)
+			{
 				return false;
-			} else {
+			}
+			else
+			{
 				return base.CanDrop(i, j, type);
 			}
 		}
