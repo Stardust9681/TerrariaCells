372d79bc36d626e86237b50f46980cc68097e7ae
Jarten <Artemy2MA@outlook.com>
2025-01-14 13:23:54 -0800

    disabled teleports to unfinished levels

--- Common/Systems/TeleportTracker.cs
+++ Common/Systems/TeleportTracker.cs
@@ -35,30 +35,30 @@ public class TeleportTracker : ModSystem
                 Main.LocalPlayer.Teleport(new(91387.836f, 7734.0f)); 
                 return;
             case 4: 
-                // hive:
-                Main.LocalPlayer.Teleport(new(47393.8f, 7158.0f)); 
-                return;
-            case 6: 
-                // crimson:
-                Main.LocalPlayer.Teleport(new(73025.14f, 7510.0f)); 
-                return;
-            case 8: 
                 // frozen:
                 Main.LocalPlayer.Teleport(new(56762.617f, 6790.0f)); 
                 return;
-            case 10: 
-                // frozen city
-                Main.LocalPlayer.Teleport(new(25136.639f, 6134.0f)); 
-                return;
-            case 12: 
-                // caverns:
-                Main.LocalPlayer.Teleport(new(28818.312f, 17606.0f)); 
-                return;
-            case 14: 
+            case 6: 
                 // forest:
                 Main.LocalPlayer.Teleport(new(12026.719f, 5990.0f)); 
                 teleports = 0;
                 return;
+            // case 4: 
+            //     // hive:
+            //     Main.LocalPlayer.Teleport(new(47393.8f, 7158.0f)); 
+            //     return;
+            // case 6: 
+            //     // crimson:
+            //     Main.LocalPlayer.Teleport(new(73025.14f, 7510.0f)); 
+            //     return;
+            // case 10: 
+            //     // frozen city
+            //     Main.LocalPlayer.Teleport(new(25136.639f, 6134.0f)); 
+            //     return;
+            // case 12: 
+            //     // caverns:
+            //     Main.LocalPlayer.Teleport(new(28818.312f, 17606.0f)); 
+            //     return;
         }
 
         Main.LocalPlayer.Teleport(new Vector2(19623f, 10326f)); //inn
