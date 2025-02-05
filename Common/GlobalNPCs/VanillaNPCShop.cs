79e4da5b57b6a36218e443bfb706afd903aeaac6
Lawro <z834li@uwaterloo.ca>
2025-02-05 17:09:39 +0000

    New NPC shop class

---
+++ Common/GlobalNPCs/VanillaNPCShop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrariaCells.Common.GlobalNPCs
+{
+    class VanillaNPCShop : GlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+
+        }
+    }
+}
