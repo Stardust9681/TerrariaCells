231e88e0d12c976bd6bfb832af77f1b2cec25adc
Stardust9681 <68380799+Stardust9681@users.noreply.github.com>
2025-02-07 13:32:04 -0600

    Revert "Added some item variables"

--- Common/GlobalItems/VanillaReworksGlobalItem.cs
+++ Common/GlobalItems/VanillaReworksGlobalItem.cs
@@ -70,51 +70,6 @@ namespace TerrariaCells.Common.GlobalItems
                     item.damage = 8;
                     item.useTime = 20;
                     break;
-                // MAGE
-                case EmeraldStaff:
-                    item.damage = 15;
-                    item.mana = 5;
-                    item.useTime = 18;
-                    item.knockBack = 0f;
-                    item.velocity = 50;
-                    break;
-                case InfernoFork:
-                    item.damage = 15;
-                    item.mana = 80;
-                    item.useTime = 45;
-                    item.knockBack = 0f;
-                    break;
-                case StaffofEarth:
-                    item.damage = 120;
-                    item.mana = 100;
-                    item.useTime = 45;
-                    item.knockBack = 10f;
-                    break;
-                case LaserRifle:
-                    item.damage = 5;
-                    item.mana = 3;
-                    item.useTime = 8;
-                    item.knockBack = 0f;
-                    break;
-                case VenomStaff:
-                    item.damage = 8;
-                    item.mana = 40;
-                    item.useTime = 30;
-                    item.knockBack = 0f;
-                    break;
-                case BookofSkulls: 
-                    item.damage = 40;
-                    item.mana = 40;
-                    item.useTime = 20;
-                    item.knockBack = 0f;
-                    item.velocity = 12;
-                    break;
-                // SUMMON
-                // Staffs
-                case ItemID.ClingerStaff:
-                    item.knockBack = 0f;
-                    break;
-
             }
         }
 
