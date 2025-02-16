2249499f9da26e7964b543ef97bff2dcc968d8a7
Jarten <Artemy2MA@outlook.com>
2025-02-15 21:44:40 -0800

    modifier system progress from remote codespace (untested)

--- Common/GlobalItems/FunkyModifiers.cs
+++ Common/GlobalItems/FunkyModifiers.cs
@@ -1,34 +1,77 @@
+// TODO: Add namespaces so that it can be built
+// I can't access the tModLoader package for building (since im away from my pc), so I cant test for it myself right now
 
 namespace TerrariaCells.Common.GlobalItems;
 
 public class FunkyModifiers : GlobalItem {
     public static Modifier[] modifiers = [
-        new Modifier(ModifierType.IncreaseDamage, 15f)
-    ]; 
+        ModifierType.IncreaseDamage.Flat(20f),
+        ModifierType.IncreaseDamage.Flat(40f, BuffID.OnFire)
+    ];
     public override void SetDefaults(Item item) {
         int modifiers = (int)(Main.rand.Next() * 4);
         
-        switch (item.type) {
+        foreach (var modifier in modifiers) {
+
         }
+    }
+
+    public override void ModifyWeaponDamage(Item item, Player player, ref StatModifier damage) {
+
     }
 }
 
 public enum ModifierType {
-    IncreaseDamage,
+    /// <summary>
+    /// if buff is given, will only give damage increase when attacking target with the buff
+    /// </summary>
+    Damage,
+    ProjectileVelocity,
+    AttackSpeed,
+    Size,
+    ManaCost,
+    /// <summary>
+    /// Increased mana consumption, massively increased damage
+    /// Can be negated for the opposite effect
+    /// </summary> 
+    ImbuedDamage,
+    DamageOnDebuff,
+    ExplosiveBullets,
+    HighVelocityBullets,
     ApplyDebuff,
-    DamageModOnBuff
+    CritsExplode,
+};
+
+/// <summary>
+/// Used to filter out modifiers that can or can't be given to specific weapons
+///
+/// These don't directly co
+/// </summary>
+public enum ModifierCategorization {
+    Swords,
+    Mana,
+
 };
 
 public static class Extensions {
     public static ModifierType minPassing = Grades.D;
-    public static Modifier Modifier(this ModifierType modifier)
+    public static Modifier Modifier(float amount, int buff = 0)
     {
+        return new Modifier() with {
+            buff = buff,
+            modifier = amount, 
+            ModifierType = modifier,
+        };
     }
-    public static Modifier Flat(this ModifierType modifier)
+    
+    public static Modifier ApplyBuff(int buff)
     {
-    }
-    public static Modifier ApplyBuff(this ModifierType modifier)
-    {
+        return new Modifier() with {
+            buff = buff,
+            flat_bonus = amount, 
+            ModifierType = modifier,
+        };
+
     }
 };
 
@@ -43,6 +86,7 @@ public struct Modifier {
         return Modifier() with
         {
             buff = buff
+
         };
     }
 
