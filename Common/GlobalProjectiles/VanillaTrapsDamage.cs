688d4d1c4415243b8371417511379d443e3f0a53
Lawro <z834li@uwaterloo.ca>
2025-02-23 00:38:52 -0500

    Change the damage of traps that shoot projectiles

---
+++ Common/GlobalProjectiles/VanillaTrapsDamage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrariaCells.Common.GlobalProjectiles
+{
+    public class VanillaTrapsDamage : GlobalProjectile
+    {
+        public override void OnSpawn(Projectile projectile, IEntitySource source)
+        {
+            int[] trapProjectiles = {
+                ProjectileID.SpearTrap, ProjectileID.Boulder, ProjectileID.MiniBoulder,
+                ProjectileID.PoisonDart, ProjectileID.PoisonDartTrap, ProjectileID.GeyserTrap,
+                ProjectileID.SpikyBallTrap, ProjectileID.FlamesTrap, ProjectileID.FlamethrowerTrap
+            };
+
+            if (trapProjectiles.Contains(projectile.type))
+            {
+                projectile.damage = 25;
+            }
+        }
+    }
+}
