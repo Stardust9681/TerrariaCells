cf919675319b5e7cc21437d2bb0a388a922b1ee3
Starwort <tcphone93@gmail.com>
2025-03-22 19:36:28 +0000

    Add system to fix rendering of actuated tree tops and leafy branches

---
+++ Common/Systems/FixTreeRendering.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.GameContent.Drawing;
+using Terraria.ModLoader;
+
+namespace TerrariaCells.Common.Systems {
+    internal class FixTreeRendering : ModSystem {
+        static bool shouldHackLighting = false;
+        public override void Load() {
+            On_Lighting.GetColor_int_int += HackLighting;
+            On_TileDrawing.DrawTrees += DetectTreeRendering;
+        }
+
+        private static void DetectTreeRendering(On_TileDrawing.orig_DrawTrees orig, TileDrawing self) {
+            shouldHackLighting = true;
+            orig(self);
+            shouldHackLighting = false;
+        }
+
+        private static Color HackLighting(On_Lighting.orig_GetColor_int_int orig, int x, int y) {
+            var result = orig(x, y);
+            if (shouldHackLighting && Main.tile[x, y].IsActuated) {
+                return new(
+                    (byte) (0.4 * result.R),
+                    (byte) (0.4 * result.G),
+                    (byte) (0.4 * result.B),
+                    result.A
+                );
+            }
+            return result;
+        }
+    }
+}
