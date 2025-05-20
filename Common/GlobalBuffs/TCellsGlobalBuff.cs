e6e738f3cda919ebbede88f86a3a53a1feadca10
Starwort <tcphone93@gmail.com>
2025-05-20 14:30:56 +0100

    Move TCellsGlobalBuff to GlobalBuffs

---
+++ Common/GlobalBuffs/TCellsGlobalBuff.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TerrariaCells.Common.GlobalBuffs
+{
+    public class TCellsGlobalBuff : GlobalBuff
+    {
+        public override void Update(int type, Player player, ref int buffIndex)
+        {
+            switch (type)
+            {
+                case BuffID.Swiftness:
+                    player.moveSpeed += 0.05f; //.25 + 0.05 => 0.3
+                    break;
+                case BuffID.Wrath:
+                    player.GetDamage(DamageClass.Generic) += 0.15f; //0.1 + 0.15 => 0.25
+                    break;
+                case BuffID.MagicPower:
+                    player.GetDamage(DamageClass.Magic) -= 0.2f; //0.2 - 0.2 => 0
+                    player.manaCost -= 0.5f;
+                    break;
+            }
+        }
+    }
+}
