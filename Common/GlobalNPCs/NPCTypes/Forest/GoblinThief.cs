f0fcc274d82d307a8b8e7aac1c3f604bc4c54912
Stardust <Stardust9681@gmail.com>
2025-06-02 14:17:59 -0500

    Goblin Thief stab attack

--- Common/GlobalNPCs/NPCTypes/Forest/GoblinThief.cs
+++ Common/GlobalNPCs/NPCTypes/Forest/GoblinThief.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using TerrariaCells.Common.GlobalNPCs.NPCTypes.Shared;
 using static TerrariaCells.Common.Utilities.NPCHelpers;
@@ -10,7 +11,16 @@ namespace TerrariaCells.Common.GlobalNPCs.NPCTypes.Forest
 	/// </remarks>
 	public class GoblinThief : AIType
 	{
-		public override bool AppliesToNPC(int npcType)
+        private ReLogic.Content.Asset<Texture2D> goblin_StabSprite;
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                goblin_StabSprite = Terraria.ModLoader.ModContent.Request<Texture2D>("TerrariaCells/Common/Assets/GoblinStab");
+            }
+            base.Load();
+        }
+        public override bool AppliesToNPC(int npcType)
 		{
 			return npcType is Terraria.ID.NPCID.GoblinThief or Terraria.ID.NPCID.Wolf;
 		}
@@ -18,6 +28,8 @@ namespace TerrariaCells.Common.GlobalNPCs.NPCTypes.Forest
 		const int Idle = 0;
 		const int ApproachTarget = 1;
 		const int Jump = 2;
+        const int Stab = 3;
+        const int Stun = 4;
 
 		const float MaxSpeed = 4f;
 		const float Accel = 0.1f;
@@ -37,12 +49,26 @@ namespace TerrariaCells.Common.GlobalNPCs.NPCTypes.Forest
 				case Jump:
 					JumpAI(npc);
 					break;
+                case Stab:
+                    StabAI(npc);
+                    break;
+                case Stun:
+                    StunAI(npc);
+                    break;
 				default:
 					npc.ai[1] = Idle;
 					break;
 			}
 			npc.spriteDirection = npc.direction;
 		}
+
+        private void ResetAI(NPC npc)
+        {
+            npc.ai[0] = 0;
+            npc.ai[1] = 0;
+            npc.ai[2] = 0;
+            npc.ai[3] = 0;
+        }
 
 		void IdleAI(NPC npc)
 		{
@@ -112,10 +138,11 @@ namespace TerrariaCells.Common.GlobalNPCs.NPCTypes.Forest
 		{
 			if (
 				!npc.TryGetTarget(out Entity target)
-				|| npc.ai[0] == 0 && !npc.TargetInAggroRange(target, lineOfSight: true)
+				|| (npc.ai[0] == 0 && !npc.TargetInAggroRange(target, lineOfSight: false))
 				|| !npc.TargetInAggroRange(target, 480, true))
 			{
-				npc.ai[1] = Idle;
+                ResetAI(npc);
+                npc.ai[1] = Idle;
                 npc.ai[3] = npc.direction;
                 return;
 			}
@@ -123,12 +150,13 @@ namespace TerrariaCells.Common.GlobalNPCs.NPCTypes.Forest
 			npc.direction = npc.velocity.X < 0 ? -1 : 1;
 			Vector2 distance = new Vector2(MathF.Abs(target.position.X - npc.position.X), MathF.Abs(target.position.Y - npc.position.Y));
 			if (
-				MathF.Abs(npc.velocity.X) > 2.8f
-				&& npc.IsFacingTarget(target)
-				&& (96 < distance.X && distance.X < 128 || 128 < distance.X && distance.X < 160 && distance.Y > 48))
+				npc.IsFacingTarget(target)
+				&& (distance.X < Utilities.NumberHelpers.ToTileDist(8) && distance.Y < 32))
 			{
-				npc.ai[1] = Jump;
-				return;
+                ResetAI(npc);
+				npc.ai[1] = Stab;
+                npc.ai[3] = (target.position.X < npc.position.X) ? -1 : 1;
+                return;
 			}
 
 			int directionToMove = target.position.X < npc.position.X ? -1 : 1;
@@ -141,10 +169,10 @@ namespace TerrariaCells.Common.GlobalNPCs.NPCTypes.Forest
 			else
 				npc.velocity.X = npc.direction * MaxSpeed;
 
-			CombatNPC.ToggleContactDamage(npc, Math.Abs(npc.velocity.X) > MaxSpeed * 0.67f);
 			if (npc.FindGroundInFront().Y > npc.Bottom.Y + npc.height * 2)
 			{
-				npc.ai[1] = Jump;
+                ResetAI(npc);
+                npc.ai[1] = Jump;
 				return;
 			}
 
@@ -155,7 +183,8 @@ namespace TerrariaCells.Common.GlobalNPCs.NPCTypes.Forest
 				Collision.StepUp(ref npc.position, ref npc.velocity, npc.width, npc.height, ref npc.stepSpeed, ref npc.gfxOffY);
 				if (oldPos == npc.position && oldVel == npc.velocity && npc.Grounded())
 				{
-					npc.ai[1] = Jump;
+                    ResetAI(npc);
+                    npc.ai[1] = Jump;
 					return;
 				}
 			}
@@ -188,25 +217,91 @@ namespace TerrariaCells.Common.GlobalNPCs.NPCTypes.Forest
 			npc.velocity.Y += 0.024f;
 			npc.ai[0]++;
 		}
+        const int Stab_Windup = 25;
+        const int Stab_DashLen = 45;
+        void StabAI(NPC npc)
+        {
+            if (!npc.TryGetTarget(out Entity target))
+            {
+                ResetAI(npc);
+                return;
+            }
+            npc.ai[0]++;
+            if (npc.ai[0] < Stab_Windup)
+            {
+                npc.velocity.X *= 0.9f;
+            }
+            else
+            {
+                CombatNPC.ToggleContactDamage(npc, true);
+                npc.velocity.X = (Stab_DashLen - (npc.ai[0]- Stab_Windup)) * 0.265f * npc.ai[3];
+                Collision.StepUp(ref npc.position, ref npc.velocity, npc.width, npc.height, ref npc.stepSpeed, ref npc.gfxOffY);
+                Collision.StepDown(ref npc.position, ref npc.velocity, npc.width, npc.height, ref npc.stepSpeed, ref npc.gfxOffY);
+                if (npc.ai[0] > Stab_DashLen + Stab_Windup || npc.collideX)
+                {
+                    ResetAI(npc);
+                    npc.ai[1] = Stun;
+                }
+            }
+        }
+        const int Stun_Delay = 25;
+        void StunAI(NPC npc)
+        {
+            CombatNPC.ToggleContactDamage(npc, false);
+            npc.ai[0]++;
+            npc.velocity.X *= 0.8f;
+
+            if (npc.ai[0] > Stun_Delay)
+            {
+                ResetAI(npc);
+                npc.ai[3] = npc.direction;
+            }
+        }
 
 		public override bool FindFrame(NPC npc, int frameHeight)
 		{
-			npc.frameCounter += (int)(npc.velocity.X);
-			if (Math.Abs(npc.frameCounter) > 5)
-			{
-				int newFrame = (npc.frame.Y / frameHeight) + Math.Sign(npc.frameCounter * npc.spriteDirection);
-				if (newFrame > Main.npcFrameCount[npc.type] - 2)
-				{
-					newFrame = 2;
-				}
-				else if (newFrame < 2)
-				{
-					newFrame = Main.npcFrameCount[npc.type] - 2;
-				}
-				npc.frame.Y = newFrame * frameHeight;
-				npc.frameCounter = 0;
-			}
-			return false;
+            if (npc.ai[1] < Stab)
+            {
+                npc.frameCounter += (int)(npc.velocity.X);
+                npc.frame.Height = frameHeight;
+                if (Math.Abs(npc.frameCounter) > 5)
+                {
+                    int newFrame = (npc.frame.Y / frameHeight) + Math.Sign(npc.frameCounter * npc.spriteDirection);
+                    if (newFrame > Main.npcFrameCount[npc.type] - 2)
+                    {
+                        newFrame = 2;
+                    }
+                    else if (newFrame < 2)
+                    {
+                        newFrame = Main.npcFrameCount[npc.type] - 2;
+                    }
+                    npc.frame.Y = newFrame * frameHeight;
+                    npc.frameCounter = 0;
+                }
+            }
+            else
+            {
+                int sheetHeight = goblin_StabSprite?.Height() ?? 1;
+                if (npc.ai[1] == Stab)
+                {
+                    npc.frame.Y = (4 * (int)npc.ai[0] / (Stab_Windup + Stab_DashLen)) * (sheetHeight / 6);
+                }
+                else
+                {
+                    npc.frame.Y = ((2 * (int)npc.ai[0] / Stun_Delay) + 4) * (sheetHeight / 6);
+                }
+            }
+            return false;
 		}
-	}
+        public override bool PreDraw(NPC npc, SpriteBatch spritebatch, Vector2 screenPos, Color lightColor)
+        {
+            if (npc.ai[1] >= Stab)
+            {
+                Vector2 size = goblin_StabSprite.Size();
+                spritebatch.Draw(goblin_StabSprite.Value, npc.Top - screenPos, npc.frame with { Width = (int)size.X, Height = (int)(size.Y / 6) } , lightColor, 0, new Vector2(size.X*0.5f, 8), npc.scale, npc.spriteDirection != 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0);
+                return false;
+            }
+            return base.PreDraw(npc, spritebatch, screenPos, lightColor);
+        }
+    }
 }
