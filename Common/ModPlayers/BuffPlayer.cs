32709e77899e03c0486bf367fd795f2688df1bb9
Stardust <Stardust9681@gmail.com>
2025-09-03 12:11:56 -0500

    Fix Magic Power not actually reducing mana cost Notes: Not sure WHY it didn't work previously

---
+++ Common/ModPlayers/BuffPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrariaCells.Common.ModPlayers
+{
+    public class BuffPlayer : ModPlayer
+    {
+        public override void ModifyManaCost(Item item, ref float reduce, ref float mult)
+        {
+            if (Player.HasBuff(BuffID.MagicPower))
+            {
+                mult *= 0;
+            }
+        }
+    }
+}
